46fc04d9f2cd7088f15cd939fdc80c4b1aaadcb5
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-02-04 17:14:33 -0500

    add userController functions

---
+++ controllers/userController.js
@@ -0,0 +1,85 @@
+const { ObjectID } = require("mongoose").Types;
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // Get all Users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Get Single User
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId }).select(
+        "-__v"
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Create User
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Delete User
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndRemove({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      const thought = await Thought.deleteMany({
+        username: user.username,
+      });
+
+      if (!thought) {
+        return res
+          .status(404)
+          .json({ message: "User deleted but no thoughts found" });
+      }
+
+      return res
+        .status(200)
+        .json({ message: "user and associated thoughts deleted" });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Update User
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  },
+};
